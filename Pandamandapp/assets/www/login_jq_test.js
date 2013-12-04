48b646fcdaa4435430b5376ef2e557a1aa484f03
Hadrien Lausson <hadrien@pandamanda.com>
2013-12-04 12:16:16 +0800

    some test code

---
+++ Pandamandapp/assets/www/login_jq_test.js
@@ -0,0 +1,28 @@
+function init() {
+	document.addEventListener("deviceready", deviceReady, true);
+	delete init;
+}
+
+function deviceReady() 
+{
+	$("#loginForm").on("submit",function(e) {
+	$("#submitButton",this).attr("disabled","disabled");
+	var u = $("#username", this).val();
+	var p = $("#password", this).val();
+	if (u != '' && p!= '')
+	{
+		$.post("http://www.coldfusionjedi.com/demos/2011/nov/10/service.cfc?method=login&returnformat=json", { username:u,password:p}, function(res) {
+			if(res == true)
+			{
+				$.mobile.changePage("thefuturishere.html");
+			}
+			else
+			{
+				navigator.notification.alert("Your login failed", function() {});
+			}
+			$("#submitButton").removeAttr("disabled");
+		},"json");
+	}
+	return false;
+	});			// remember to change the code later...
+}
